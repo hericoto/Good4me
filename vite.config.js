f4708b2de4608c98137ab71a3313f4dfa23da141
Дарья Епанчинцева <epandasha@gmail.com>
2025-09-06 21:07:53 +0700

    инициализирует проект

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/test21t/',
+  base: '/Good4me/',
   build: {
     emptyOutDir: true,
     modulePreload: { polyfill: false },
